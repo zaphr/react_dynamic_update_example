76ced258c7aeda3320da5e5c7a3ea3014c812342
Anthony Reinbach <areinbach@areinbach.com>
2016-08-22 18:13:41 +1000

    Pair back to a barebones webpack config for simplicity

---
+++ app/main.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Flower ka-power
+      </div>
+    );
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,22 @@
-var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
 var path = require('path');
 
+var BUILD = path.resolve(__dirname, 'public');
+var APP = path.resolve(__dirname, 'app');
+
 module.exports = {
-  context: path.join(__dirname, "src"),
-  devtool: debug ? "inline-sourcemap" : null,
-  entry: "./js/App.js",
-  devServer: {
-    inline: true,
-    port: 3333
+  entry: APP + '/main.jsx',
+  output: {
+    path: BUILD,
+    filename: 'bundle.js'
   },
-  module: {
-    loaders: [
+  module : {
+    loaders : [
       {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'react-hmre'],
-          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
-        }
+        test : /\.jsx?/,
+        include : APP,
+        loader : 'babel'
       }
     ]
-  },
-  output: {
-    path: __dirname + "/src/",
-    filename: "bundle.min.js"
-  },
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
+  }
 };
