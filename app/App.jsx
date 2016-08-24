6543dc52009f088a387f2f78527a94d8e1df1649
Anthony Reinbach <areinbach@areinbach.com>
2016-08-24 18:37:54 +1000

    Start on basic layout

--- app/App.jsx
+++ app/App.jsx
@@ -21,8 +21,8 @@ export default class App extends React.Component {
     render() {
         return (
             <div>
-                <OrderEntry/>
                 <BundleDisplay bundles={this.state.bundles}/>
+                <OrderEntry/>
             </div>
         );
     }
