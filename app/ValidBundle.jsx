74205177c47d4f367176deaa2c5135bda60033b1
Anthony Reinbach <areinbach@areinbach.com>
2016-08-24 15:42:04 +1000

    Render order summary and item lines

--- app/ValidBundle.jsx
+++ app/ValidBundle.jsx
@@ -3,10 +3,25 @@ import React, { PropTypes } from 'react'
 export default class ValidBundle extends React.Component {
 
     render() {
+        let bundle = this.props.bundle
+        let bundleItems = Object.keys(bundle.bundleCount).map((bundleMin, index)=> {
+            if (! bundle.bundleData[bundleMin]) {
+                return <li key={index}>{bundle.bundleCount[bundleMin]} x {bundleMin}  **** </li>
+            } else {
+                return <li key={index}>{bundle.bundleCount[bundleMin]} x {bundleMin}
+                ${bundle.bundleCount[bundleMin] * bundle.bundleData[bundleMin]}  </li>
+            }
+        })
 
         return (
             <div className="validBundle">
-                {this.props.bundle.lineText}
+              <div className="orderSummary">
+                    {this.props.bundle.count} {this.props.bundle.code} ${this.props.bundle.orderTotal}
+              </div>
+                <ul>
+                    {bundleItems}
+                </ul>
+
             </div>
         );
     }
