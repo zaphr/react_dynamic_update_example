771e26698db3220400603d5b5c5f031f9fa985ed
Anthony Reinbach <areinbach@areinbach.com>
2016-08-24 17:39:39 +1000

    Find valid bundle combinations

--- app/Store.jsx
+++ app/Store.jsx
@@ -1,5 +1,4 @@
 import {createStore } from 'redux'
-import OrderUtils from './OrderUtil.jsx'
 
 const intialState = {
     stockBundles: {
@@ -37,12 +36,16 @@ const flowerStore = (state = intialState, action) => {
         case 'USER_ENTRY':
             var orders = []
             action.value.forEach((line) => {
-                let order = OrderUtils.orderForLine(line, state.stockBundles)
+                let order = orderForLine(line, state.stockBundles)
                 if (order.valid) {
                     order.bundleData = state.stockBundles[order.code].bundles
                     order.name = state.stockBundles[order.code].name
-                    order.bundleCount = OrderUtils.bundlesForOrder(order, order.bundleData)
-                    order.orderTotal = OrderUtils.orderTotal(order)
+                    order.bundleCount = findValidBundleCombination(order, order.bundleData)
+                    if (order.bundleCount){
+                        order.orderTotal = orderTotal(order)
+                    } else {
+                        order.valid = false
+                    }
                 }
                 orders.push(order)
             })
@@ -54,5 +57,77 @@ const flowerStore = (state = intialState, action) => {
 
 export default createStore(flowerStore)
 
+////////
 
+
+const orderForLine = (lineText, bundleData) => {
+    var order = {
+        valid: false,
+        lineText: lineText
+    }
+
+    let textSections = lineText.trim().split(" ").filter((line) => line.length != 0)
+
+    if (textSections.length != 2) {
+        return order
+    }
+
+    order.count = textSections[0]
+    order.code = textSections[1]
+
+    let validOrderQuantity = /^[0-9]+$/.test(order.count)
+    if (validOrderQuantity) {
+        order.count = parseInt(order.count, 10)
+    }
+    let validOrderId = Object.keys(bundleData).filter((productID) => productID == order.code).length > 0
+
+    order.valid = validOrderQuantity && validOrderId
+    return order
+}
+
+const findValidBundleCombination = (order, bundleData) => {
+    // Sort bundle counts in descending order
+    var bundleKeys =  Object.keys(bundleData).sort((a, b)=> {
+        return a - b * -1 })
+    let totalItems = bundleKeys.length
+
+    for (var i=0; i < totalItems; i++){
+        let bundleCount = bundlesForOrder(order, bundleKeys)
+        if (bundleCount) {
+            return bundleCount
+        } else {
+            bundleKeys.shift()
+        }
+    }
+    return false
+}
+
+const bundlesForOrder = (order, bundleData) => {
+
+    var bundleCount = {}
+    var orderCountRemaining = order.count
+
+    bundleData.forEach((bundleCountMin) => {
+        let countForBundle = Math.floor(orderCountRemaining / bundleCountMin)
+        if (countForBundle > 0) {
+            bundleCount[bundleCountMin] = countForBundle
+            orderCountRemaining = orderCountRemaining - (countForBundle * bundleCountMin)
+        }
+    })
+
+    if (orderCountRemaining > 0) {
+        return false
+    } else {
+        return bundleCount
+    }
+}
+
+const orderTotal = (order) => {
+    var orderTotal = 0.0
+    // Ignore singles in total until requirements and values known
+    Object.keys(order.bundleCount).filter((countType) => countType != 1).forEach((bundleCountMin) => {
+        orderTotal = orderTotal + (order.bundleCount[bundleCountMin] * order.bundleData[bundleCountMin])
+    })
+    return orderTotal
+}
 
