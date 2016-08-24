10e1c02849b9472b1d2715fc6cce4119dcd9cc34
Anthony Reinbach <areinbach@areinbach.com>
2016-08-24 19:52:19 +1000

    Ok jest is a mess, mocha looks much better choice

--- webpack.config.js
+++ webpack.config.js
@@ -8,15 +8,75 @@ module.exports = {
   entry: APP + '/main.jsx',
   output: {
     path: BUILD,
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: 'http://localhost:7700/dist'
   },
-  module : {
-    loaders : [
-      {
-        test : /\.jsx?/,
-        include : APP,
-        loader : 'babel'
-      }
-    ]
-  }
+  devServer: {
+    hot: true,
+    inline: true,
+    port: 7700,
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  externals: {
+    'cheerio': 'window',
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true,
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-0'],
+      },
+    }],
+  },
+  plugins: []
+
 };
+
+
+//
+// const path = require('path');
+// const webpack = require('webpack');
+//
+// // env
+// const buildDirectory = './dist/';
+//
+// module.exports = {
+//   entry: './lib/main.jsx',
+//   devServer: {
+//     hot: true,
+//     inline: true,
+//     port: 7700,
+//     historyApiFallback: true,
+//   },
+//   resolve: {
+//     extensions: ['', '.js', '.jsx'],
+//   },
+//   output: {
+//     path: path.resolve(buildDirectory),
+//     filename: 'app.js',
+//     publicPath: 'http://localhost:7700/dist',
+//   },
+//   externals: {
+//     'cheerio': 'window',
+//     'react/lib/ExecutionEnvironment': true,
+//     'react/lib/ReactContext': true,
+//   },
+//   module: {
+//     loaders: [{
+//       test: /\.jsx?$/,
+//       exclude: /(node_modules|bower_components)/,
+//       loader: 'babel',
+//       query: {
+//         presets: ['react', 'es2015', 'stage-0'],
+//       },
+//     }],
+//   },
+//   plugins: [],
+// };
